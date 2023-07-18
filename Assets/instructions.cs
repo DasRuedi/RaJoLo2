265318a838e22d2c4387d98ec2d0f3f57d78876f
DasRuedi <r_l.fiedler@web.de>
2023-07-18 18:03:37 +0200

    Day 16 submit
    Imma head out

--- Assets/instructions.cs
+++ Assets/instructions.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class instructions : MonoBehaviour
 {
     public GameObject cam;
+    public GameObject gameManager;
+    public GameObject okButton;
+    public GameObject deleteButton;
+
 
     public Sprite[] sprites;
     public int spriteChoice;
@@ -15,6 +19,9 @@ public class instructions : MonoBehaviour
     public bool bubble;
     public bool anger;
 
+    public bool delete;
+    public bool ok;
+
 
     public float swapTime;
     public float swapRate;
@@ -22,9 +29,12 @@ public class instructions : MonoBehaviour
     public bool smolInstruction;
     public bool bigInstruction;
 
+    public Vector3 startPos;
+    public Vector3 inactPos;
+
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
 
@@ -77,6 +87,83 @@ public class instructions : MonoBehaviour
         {
             if (smolInstruction == true)
             {
+                if (gameManager.GetComponent<miniGameManager>().state == GameState.DRAWING)
+                {
+                    if (delete == false || ok == false)
+                    {
+                        transform.position = startPos;
+                        swapTime += Time.deltaTime;
+
+                        if (swapTime >= swapRate)
+                        {
+                            spriteChoice++;
+                            swapTime = 0;
+                        }
+
+                        if (spriteChoice >= 3)
+                        {
+                            spriteChoice = 0;
+                        }
+                    }
+
+                    if (delete == true)
+                    {
+                        if (deleteButton.GetComponent<jiggle>().hoverOver == true)
+                        {
+                            Debug.Log("wigglewiggle");
+                            transform.position = startPos;
+                            swapTime += Time.deltaTime;
+
+                            if (swapTime >= swapRate)
+                            {
+                                spriteChoice++;
+                                swapTime = 0;
+                            }
+
+                            if (spriteChoice >= 3)
+                            {
+                                spriteChoice = 0;
+                            }
+                        }
+                        if (deleteButton.GetComponent<jiggle>().hoverOver == false)
+                        {
+                            transform.position = inactPos;
+                        }
+                    }
+                    if (ok == true)
+                    {
+                        if (okButton.GetComponent<jiggle>().hoverOver == true)
+                        {
+                            transform.position = startPos;
+                            swapTime += Time.deltaTime;
+
+                            if (swapTime >= swapRate)
+                            {
+                                spriteChoice++;
+                                swapTime = 0;
+                            }
+
+                            if (spriteChoice >= 3)
+                            {
+                                spriteChoice = 0;
+                            }
+                        }
+                        if (okButton.GetComponent<jiggle>().hoverOver == false)
+                        {
+                            transform.position = inactPos;
+                        }
+                    }
+
+                }
+                if (gameManager.GetComponent<miniGameManager>().state != GameState.DRAWING)
+                {
+                    transform.position = inactPos;
+                }
+
+            }
+
+            if (bigInstruction == true)
+            {
                 swapTime += Time.deltaTime;
 
                 if (swapTime >= swapRate)
@@ -90,8 +177,6 @@ public class instructions : MonoBehaviour
                     spriteChoice = 0;
                 }
             }
-
-
         }
     }
 }
